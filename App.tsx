2c65437cac660292be0ae710c177ef258c9cba28
Qulbe12 <qhussain@intelligentsoftware.net>
2023-02-15 03:43:26 +0500

    Feat/Stack

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NativeBaseProvider} from "native-base";
+import Main from "./src/Main";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <NativeBaseProvider>
+                <Main/>
+            </NativeBaseProvider>
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/account/UserAddress.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Box, Button, Center, FormControl, Heading, HStack, Input, Link, Text, View, VStack} from "native-base";
+import {useNavigation} from "@react-navigation/native";
+
+const UserAddress = () => {
+
+    const navigation = useNavigation()
+
+    return (
+        <Center w="100%">
+            <Box safeArea p="2" py="8" w="90%" maxW="290">
+                <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
+                    color: "warmGray.50"
+                }}>
+                    Give your address details
+                </Heading>
+                <VStack space={3} mt="5">
+                    <FormControl>
+                        <FormControl.Label>Street Number</FormControl.Label>
+                        <Input />
+                    </FormControl>
+                    <FormControl>
+                        <FormControl.Label>Street Name</FormControl.Label>
+                        <Input />
+                    </FormControl><FormControl>
+                        <FormControl.Label>City</FormControl.Label>
+                        <Input />
+                    </FormControl><FormControl>
+                        <FormControl.Label>Country</FormControl.Label>
+                        <Input />
+                    </FormControl><FormControl>
+                        <FormControl.Label>Phone Number</FormControl.Label>
+                        <Input />
+                    </FormControl>
+                    <Button mt="2" colorScheme="indigo" onPress={()=>{
+                        navigation.navigate("User's Card Info" as never)
+                    }}>
+                        Next
+                    </Button>
+                </VStack>
+            </Box>
+        </Center>
+    );
+};
+
+export default UserAddress;
