5878ddcdf01d90da37d7c5040ce077cd6ab4a352
Qulbe12 <qhussain@intelligentsoftware.net>
2023-02-16 01:45:14 +0500

    feat: sign up page designed

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import SignIn from "./account/signIn";
+import SignIn from "./account/SignIn";
 import { createStackNavigator } from "@react-navigation/stack";
 import UserAddress from "./account/UserAddress";
 import UserInfo from "./account/UserInfo";
 import UserCardInfo from "./account/UserCarsdInfo";
+import SignUp from "./account/SignUp";
 
 const Main = () => {
     const Stack = createStackNavigator()
@@ -13,6 +14,7 @@ const Main = () => {
     <Stack.Screen name="User Address" options={{title: "User Address", headerTitleAlign: "center"}} component={UserAddress}/>
     <Stack.Screen name="User Info" options={{title: "User Info", headerTitleAlign: "center"}} component={UserInfo}/>
     <Stack.Screen name="User's Card Info" options={{title: "User's Card Info", headerTitleAlign: "center"}} component={UserCardInfo}/>
+    <Stack.Screen name="SignUp" options={{title: "Sign Up", headerTitleAlign: "center"}} component={SignUp}/>
         </Stack.Navigator>
     );
 };
